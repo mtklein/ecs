5cab687e608b888c42c6efb116715636f577cb10
Mike Klein <mtklein@gmail.com>
2025-07-06 10:53:02 +0200

    style

--- ecs.c
+++ ecs.c
@@ -3,104 +3,104 @@
 #include <string.h>
 
 struct branch {
-    struct branch *left;
-    struct branch *right;
-    int            begin;
-    int            end;
-    int            height;
-    int            :32;
-    char           data[];
+    struct branch *left,*right;
+    int  height;
+    int  begin,end;
+    int  :32;
+    char data[];
 };
 
-static int height(struct branch *n) { return n ? n->height : 0; }
-
-static void update(struct branch *n) {
-    int hl = height(n->left);
-    int hr = height(n->right);
-    n->height = hl > hr ? hl + 1 : hr + 1;
+static int height(struct branch *b) {
+    return b ? b->height : 0;
 }
 
-static struct branch *rotate_right(struct branch *y) {
+static void recalculate_height(struct branch *b) {
+    int const l = height(b->left ),
+              r = height(b->right);
+    b->height = (l > r ? l : r) + 1;
+}
+
+static struct branch* rotate_right(struct branch *y) {
     struct branch *x = y->left;
-    y->left = x->right;
+    y->left  = x->right;
     x->right = y;
-    update(y);
-    update(x);
+    recalculate_height(y);
+    recalculate_height(x);
     return x;
 }
 
-static struct branch *rotate_left(struct branch *x) {
+static struct branch* rotate_left(struct branch *x) {
     struct branch *y = x->right;
     x->right = y->left;
-    y->left = x;
-    update(x);
-    update(y);
+    y->left  = x;
+    recalculate_height(x);
+    recalculate_height(y);
     return y;
 }
 
-static struct branch *balance(struct branch *n) {
-    update(n);
-    int bal = height(n->left) - height(n->right);
+static struct branch* balance(struct branch *b) {
+    recalculate_height(b);
+    int const bal = height(b->left) - height(b->right);
     if (bal > 1) {
-        if (height(n->left->right) > height(n->left->left)) {
-            n->left = rotate_left(n->left);
+        if (height(b->left->right) > height(b->left->left)) {
+            b->left = rotate_left(b->left);
         }
-        return rotate_right(n);
+        return rotate_right(b);
     }
     if (bal < -1) {
-        if (height(n->right->left) > height(n->right->right)) {
-            n->right = rotate_right(n->right);
+        if (height(b->right->left) > height(b->right->right)) {
+            b->right = rotate_right(b->right);
         }
-        return rotate_left(n);
+        return rotate_left(b);
     }
-    return n;
+    return b;
 }
 
-static struct branch *avl_insert(struct branch *root, struct branch *node) {
-    if (!root) {
-        return node;
+static struct branch* avl_insert(struct branch *root, struct branch *node) {
+    if (root) {
+        if (node->begin < root->begin) {
+            root->left = avl_insert(root->left, node);
+        } else {
+            root->right = avl_insert(root->right, node);
+        }
+        return balance(root);
     }
-    if (node->begin < root->begin) {
-        root->left = avl_insert(root->left, node);
-    } else {
-        root->right = avl_insert(root->right, node);
-    }
-    return balance(root);
+    return node;
 }
 
-static struct branch *avl_remove_min(struct branch *n, struct branch **out) {
-    if (!n->left) {
-        *out = n;
-        return n->right;
+static struct branch* avl_remove_min(struct branch *b, struct branch **out) {
+    if (b->left) {
+        b->left = avl_remove_min(b->left, out);
+        return balance(b);
     }
-    n->left = avl_remove_min(n->left, out);
-    return balance(n);
+    *out = b;
+    return b->right;
 }
 
-static struct branch *avl_remove(struct branch *root, int key) {
-    if (!root) {
-        return NULL;
-    }
-    if (key < root->begin) {
-        root->left = avl_remove(root->left, key);
-    } else if (key > root->begin) {
-        root->right = avl_remove(root->right, key);
-    } else {
-        struct branch *left_subtree = root->left;
-        struct branch *right_subtree = root->right;
-        if (!right_subtree) {
+static struct branch* avl_remove(struct branch *root, int key) {
+    if (root) {
+        if (key < root->begin) {
+            root->left = avl_remove(root->left, key);
+        } else if (key > root->begin) {
+            root->right = avl_remove(root->right, key);
+        } else {
+            struct branch *left_subtree = root->left,
+                         *right_subtree = root->right;
+            if (right_subtree) {
+                struct branch *min;
+                right_subtree = avl_remove_min(right_subtree, &min);
+                min->left  = left_subtree;
+                min->right = right_subtree;
+                return balance(min);
+            }
             return left_subtree;
         }
-        struct branch *min;
-        right_subtree = avl_remove_min(right_subtree, &min);
-        min->left = left_subtree;
-        min->right = right_subtree;
-        return balance(min);
+        return balance(root);
     }
-    return balance(root);
+    return NULL;
 }
 
-static struct branch *branch_find(struct branch *root, int entity) {
+static struct branch* branch_find(struct branch *root, int entity) {
     while (root) {
         if (entity < root->begin) {
             root = root->left;
@@ -113,7 +113,7 @@ static struct branch *branch_find(struct branch *root, int entity) {
     return NULL;
 }
 
-static struct branch *branch_find_lt(struct branch *root, int entity) {
+static struct branch* branch_find_lt(struct branch *root, int entity) {
     struct branch *best = NULL;
     while (root) {
         if (entity <= root->begin) {
@@ -126,7 +126,7 @@ static struct branch *branch_find_lt(struct branch *root, int entity) {
     return best;
 }
 
-static struct branch *branch_find_gt(struct branch *root, int entity) {
+static struct branch* branch_find_gt(struct branch *root, int entity) {
     struct branch *best = NULL;
     while (root) {
         if (entity < root->begin) {
@@ -139,21 +139,19 @@ static struct branch *branch_find_gt(struct branch *root, int entity) {
     return best;
 }
 
-static struct branch *branch_new(int begin, int end, size_t size) {
-    struct branch *n = malloc(sizeof(*n) + (size_t)(end - begin) * size);
-    n->left = n->right = NULL;
-    n->height = 1;
-    n->begin = begin;
-    n->end = end;
-    memset(n->data, 0, (size_t)(end - begin) * size);
-    return n;
+static struct branch* branch_new(int begin, int end, size_t size) {
+    struct branch *b = calloc(1, sizeof *b + (size_t)(end - begin) * size);
+    b->height = 1;
+    b->begin  = begin;
+    b->end    = end;
+    return b;
 }
 
-static void *branch_ptr(struct branch *n, size_t size, int entity) {
-    return n->data + (size_t)(entity - n->begin) * size;
+static void* branch_ptr(struct branch *b, size_t size, int entity) {
+    return b->data + (size_t)(entity - b->begin) * size;
 }
 
-static struct branch *component_insert_branch(struct component *c, struct branch *branch) {
+static struct branch* component_insert_branch(struct component *c, struct branch *branch) {
     c->root = avl_insert(c->root, branch);
     return branch;
 }
@@ -163,10 +161,10 @@ static void component_remove_branch(struct component *c, struct branch *branch) 
     free(branch);
 }
 
-void *component_data(struct component *c, int entity) {
-    struct branch *n = branch_find(c->root, entity);
-    if (n) {
-        return branch_ptr(n, c->size, entity);
+void* component_data(struct component *c, int entity) {
+    struct branch *b = branch_find(c->root, entity);
+    if (b) {
+        return branch_ptr(b, c->size, entity);
     }
 
     struct branch *pred = branch_find_lt(c->root, entity);
@@ -186,9 +184,9 @@ void *component_data(struct component *c, int entity) {
             component_remove_branch(c, newl);
             component_remove_branch(c, succ);
             component_insert_branch(c, merge);
-            n = merge;
+            b = merge;
         } else {
-            n = newl;
+            b = newl;
         }
     } else if (succ && entity + 1 == succ->begin) {
         struct branch *newl = branch_new(entity, succ->end, c->size);
@@ -208,70 +206,62 @@ void *component_data(struct component *c, int entity) {
             component_remove_branch(c, pred);
             component_remove_branch(c, newl);
             component_insert_branch(c, merge);
-            n = merge;
+            b = merge;
         } else {
-            n = newl;
+            b = newl;
         }
     } else {
-        n = branch_new(entity, entity + 1, c->size);
-        component_insert_branch(c, n);
+        b = branch_new(entity, entity + 1, c->size);
+        component_insert_branch(c, b);
     }
-    return branch_ptr(n, c->size, entity);
+    return branch_ptr(b, c->size, entity);
 }
 
 void component_drop(struct component *c, int entity) {
-    struct branch *n = branch_find(c->root, entity);
-    if (!n) {
-        return;
-    }
+    struct branch *b = branch_find(c->root, entity);
+    if (b) {
+        if (b->begin == entity && b->end == entity + 1) {
+            component_remove_branch(c, b);
+            return;
+        }
 
-    if (n->begin == entity && n->end == entity + 1) {
-        component_remove_branch(c, n);
-        return;
-    }
+        if (entity == b->begin) {
+            struct branch *newl = branch_new(entity + 1, b->end, c->size);
+            memcpy(newl->data, b->data + c->size, (size_t)(b->end - b->begin - 1) * c->size);
+            component_remove_branch(c, b);
+            component_insert_branch(c, newl);
+            return;
+        }
 
-    if (entity == n->begin) {
-        struct branch *newl = branch_new(entity + 1, n->end, c->size);
-        memcpy(newl->data, n->data + c->size, (size_t)(n->end - n->begin - 1) * c->size);
-        component_remove_branch(c, n);
-        component_insert_branch(c, newl);
-        return;
-    }
+        if (entity == b->end - 1) {
+            struct branch *newl = branch_new(b->begin, b->end - 1, c->size);
+            memcpy(newl->data, b->data, (size_t)(b->end - b->begin - 1) * c->size);
+            component_remove_branch(c, b);
+            component_insert_branch(c, newl);
+            return;
+        }
 
-    if (entity == n->end - 1) {
-        struct branch *newl = branch_new(n->begin, n->end - 1, c->size);
-        memcpy(newl->data, n->data, (size_t)(n->end - n->begin - 1) * c->size);
-        component_remove_branch(c, n);
-        component_insert_branch(c, newl);
-        return;
+        struct branch *right = branch_new(entity + 1, b->end, c->size);
+        memcpy(right->data, branch_ptr(b, c->size, entity + 1),
+               (size_t)(b->end - (entity + 1)) * c->size);
+        struct branch *left = branch_new(b->begin, entity, c->size);
+        memcpy(left->data, b->data, (size_t)(entity - b->begin) * c->size);
+        component_remove_branch(c, b);
+        component_insert_branch(c, left);
+        component_insert_branch(c, right);
     }
-
-    struct branch *right = branch_new(entity + 1, n->end, c->size);
-    memcpy(right->data, branch_ptr(n, c->size, entity + 1),
-           (size_t)(n->end - (entity + 1)) * c->size);
-    struct branch *left = branch_new(n->begin, entity, c->size);
-    memcpy(left->data, n->data, (size_t)(entity - n->begin) * c->size);
-    component_remove_branch(c, n);
-    component_insert_branch(c, left);
-    component_insert_branch(c, right);
 }
 
-static void each_branch(struct branch *n,
-                      void (*fn)(int, void *, void *),
-                      void *ctx,
-                      size_t size) {
-    if (!n) {
-        return;
+static void each_branch(struct branch *b, void (*fn)(int, void *, void *), void *ctx, size_t size) {
+    if (b) {
+        each_branch(b->left, fn, ctx, size);
+        for (int i = b->begin; i < b->end; i++) {
+            fn(i, b->data + (size_t)(i - b->begin) * size, ctx);
+        }
+        each_branch(b->right, fn, ctx, size);
     }
-    each_branch(n->left, fn, ctx, size);
-    for (int i = n->begin; i < n->end; ++i) {
-        fn(i, n->data + (size_t)(i - n->begin) * size, ctx);
-    }
-    each_branch(n->right, fn, ctx, size);
 }
 
-void component_each(struct component *c,
-                    void (*fn)(int entity, void *data, void *ctx),
-                    void *ctx) {
+void component_each(struct component *c, void (*fn)(int entity, void *data, void *ctx), void *ctx) {
     each_branch(c->root, fn, ctx, c->size);
 }
