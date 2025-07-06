cbac13a7515bb1dbfd2e7fc831df15746dcc4186
Mike Klein <mtklein@gmail.com>
2025-07-06 13:22:47 +0200

    undo branch_copy

--- ecs.c
+++ ecs.c
@@ -165,14 +165,6 @@ static void* branch_ptr(struct branch *b, size_t size, int i) {
     return b->data + (size_t)(i - b->begin) * size;
 }
 
-static void branch_copy(struct branch *dst, struct branch const *src, size_t size,
-                        int begin, int end) {
-    memcpy(dst->data,
-           src->data + (size_t)(begin - src->begin) * size,
-           (size_t)(end - begin) * size);
-}
-
-
 static struct branch* component_insert_branch(struct component *c, struct branch *branch) {
     c->root = avl_insert(c->root, branch);
     return branch;
@@ -251,7 +243,7 @@ void component_drop(struct component *c, int i) {
 
         if (b->begin == i) {
             struct branch *shrunk = branch_new(i+1, b->end, c->size, 0);
-            branch_copy(shrunk,b,c->size, b->begin+1,b->end);
+            memcpy(shrunk->data, branch_ptr(b,c->size,i+1), (size_t)(b->end - (i+1)) * c->size);
             component_remove_branch(c, b);
             component_insert_branch(c, shrunk);
             return;
@@ -261,7 +253,7 @@ void component_drop(struct component *c, int i) {
             b->end -= 1;
             if (b->end - b->begin <= b->cap / 4 && b->cap > 1) {
                 struct branch *shrunk = branch_new(b->begin, b->end, c->size, b->cap/2);
-                branch_copy(shrunk,b,c->size, b->begin,b->end);
+                memcpy(shrunk->data, b->data, (size_t)(b->end - b->begin) * c->size);
                 component_remove_branch(c, b);
                 component_insert_branch(c, shrunk);
             }
@@ -270,8 +262,8 @@ void component_drop(struct component *c, int i) {
 
         struct branch *L = branch_new(b->begin, i, c->size, 0);
         struct branch *R = branch_new(i+1, b->end, c->size, 0);
-        branch_copy(L,b,c->size, b->begin, i);
-        branch_copy(R,b,c->size, i+1, b->end);
+        memcpy(L->data, branch_ptr(b,c->size,b->begin), (size_t)(i  -  b->begin) * c->size);
+        memcpy(R->data, branch_ptr(b,c->size,     i+1), (size_t)(b->end - (i+1)) * c->size);
         component_remove_branch(c, b);
         component_insert_branch(c, L);
         component_insert_branch(c, R);
