2b9fba008765ff2a6f9d8aece8144cb543cb942e
Mike Klein <mtklein@gmail.com>
2025-07-23 14:41:32 -0500

    revise

--- ecs_test.c
+++ ecs_test.c
@@ -1,67 +1,110 @@
 #include "ecs.h"
-#include <assert.h>
-#include <string.h>
-#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-struct ent {
-    int ix;
+#define expect(x) \
+    if (!(x)) fprintf(stderr, "%s:%d expect(%s)\n", __FILE__, __LINE__, #x), __builtin_debugtrap()
+#define TODO(x) expect(!(x))
+
+static void free_data(void *p) {
+    array *arr = p;
+    free(arr->data);
+    arr->data = NULL;
+}
+
+struct entity {
+    int a,b;
 };
 
-int main(void) {
-    array entity   = {.size = sizeof(struct ent)};
-    array freelist = {.size = sizeof(int)};
+static void test_ids(void) {
+    __attribute__((cleanup(free_data)))
+    array entity   = {.size = sizeof(struct entity)},
+          freelist = {.size = sizeof(int)};
 
-    int const id0 = alloc_id(&entity,&freelist);
-    assert(id0 == 0);
-    assert(entity.n == 1);
-    uint8_t *mem = entity.data;
-    for (size_t i = 0; i < sizeof(struct ent); i++) {
-        assert(mem[i] == 0xff);
+    int const id = alloc_id(&entity, &freelist);
+    {
+        expect(      id == 0);
+        expect(entity.n == 1);
+
+        struct entity *e = ptr(&entity, id);
+        expect(e->a == ~0);
+        expect(e->b == ~0);
+        e->a = 42;
+        e->b = 47;
     }
 
-    int const id1 = alloc_id(&entity,&freelist);
-    assert(id1 == 1);
-    assert(entity.n == 2);
+    int const next = alloc_id(&entity, &freelist);
+    {
+        expect(    next == 1);
+        expect(entity.n == 2);
 
-    drop_id(&entity,&freelist,id0);
-    mem = (uint8_t *)ptr(&entity,id0);
-    for (size_t i = 0; i < sizeof(struct ent); i++) {
-        assert(mem[i] == 0xff);
+        struct entity *e = ptr(&entity, next);
+        expect(e->a == ~0);
+        expect(e->b == ~0);
+        e->a = 12;
+        e->b = 17;
     }
-    assert(freelist.n == 1);
-    assert(*(int *)ptr(&freelist,0) == id0);
 
-    int const id2 = alloc_id(&entity,&freelist);
-    assert(id2 == id0);
-    assert(freelist.n == 0);
+    drop_id(&entity, &freelist, id);
+    {
+        expect(entity.n == 2);
+        struct entity const *e = entity.data;
+        expect(e[  id].a == ~0);
+        expect(e[  id].b == ~0);
+        expect(e[next].a == 12);
+        expect(e[next].b == 17);
 
+        expect(freelist.n == 1);
+        int const *free_id = freelist.data;
+        expect(*free_id == id);
+    }
+
+    int const reuse = alloc_id(&entity, &freelist);
+    {
+        expect(     reuse == id);
+        expect(  entity.n ==  2);
+        expect(freelist.n ==  0);
+
+        struct entity const *e = entity.data;
+        expect(e[reuse].a == ~0);
+        expect(e[reuse].b == ~0);
+        expect(e[ next].a == 12);
+        expect(e[ next].b == 17);
+    }
+}
+
+static void test_components(void) {
+    __attribute__((cleanup(free_data)))
     array comp = {.size = sizeof(int)};
+
     int ix = -1;
-    int v = 1;
-    component_set(&comp,&ix,&v);
-    assert(ix == 0);
-    assert(comp.n == 1);
-    assert(*(int *)component_get(&comp,ix) == 1);
+    int val = 1;
+    component_set(&comp, &ix, &val);
+    expect(    ix == 0);
+    expect(comp.n == 1);
+    expect(*(int*)component_get(&comp, ix) == val);
 
-    v = 2;
-    component_set(&comp,&ix,&v);
-    assert(ix == 0);
-    assert(comp.n == 1);
-    assert(*(int *)component_get(&comp,ix) == 2);
+    val = 2;
+    component_set(&comp, &ix, &val);
+    expect(    ix == 0);
+    expect(comp.n == 1);
+    expect(*(int*)component_get(&comp, ix) == val);
 
-    component_del(&comp,&ix);
-    assert(ix == ~0);
-    assert(component_get(&comp,ix) == NULL);
+    component_del(&comp, &ix);
+    expect(ix == ~0);
+    expect(component_get(&comp, ix) == NULL);
 
-    v = 3;
-    component_set(&comp,&ix,&v);
-    assert(ix == 1);
-    assert(comp.n == 2);
-    assert(*(int *)component_get(&comp,ix) == 3);
+    val = 3;
+    component_set(&comp, &ix, &val);
+    expect(    ix == 1);
+    expect(comp.n == 2);
+    TODO(      ix == 0);
+    TODO(  comp.n == 1);
+    expect(*(int*)component_get(&comp,ix) == val);
+}
 
-    free(entity.data);
-    free(freelist.data);
-    free(comp.data);
+int main(void) {
+    test_ids();
+    test_components();
     return 0;
 }
