acfedd6fca834916c519c1fcad8216dc919cfa75
Mike Klein <mtklein@gmail.com>
2025-07-30 09:26:33 -0400

    deck chairs

--- ecs.h
+++ ecs.h
@@ -6,6 +6,10 @@ struct component {
     int  *id,*ix;
     int   n,cap;
 };
+void* component_attach_(struct component      *, size_t, int id);
+void  component_detach_(struct component      *, size_t, int id);
+void* component_lookup_(struct component const*, size_t, int id);
+
 #define component(T) \
     union { struct {T *data; int *id,*ix; int n,cap; }; struct component type_erased; }
 
@@ -14,7 +18,3 @@ struct component {
 #define component_detach(c,id) component_detach_(&(c)->type_erased, sizeof *(c)->data, id)
 #define component_lookup(c,id) \
        (__typeof__((c)->data)) component_lookup_(&(c)->type_erased, sizeof *(c)->data, id)
-
-void* component_attach_(struct component      *, size_t, int id);
-void  component_detach_(struct component      *, size_t, int id);
-void* component_lookup_(struct component const*, size_t, int id);
