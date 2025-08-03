976677ad37142e6e9598dbf503f806fc3c761de4
Mike Klein <mtklein@gmail.com>
2025-08-03 19:04:30 -0400

    Inline table component logic (#76)
    * Inline table component logic
    
    * fine without

--- table.c
+++ table.c
@@ -1,8 +1,54 @@
 #include "table.h"
-#include "ecs.h"
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct component {
+    void *data;
+    int  *id,*ix;
+    int   n,cap;
+};
+
+static int max(int x, int y) {
+    return x > y ? x : y;
+}
+
+static _Bool is_pow2_or_zero(int x) {
+    return (x & (x-1)) == 0;
+}
+
+static void* component_attach_(struct component *c, size_t size, int id) {
+    if (id >= c->cap) {
+        int const grown = max(id+1, 2*c->cap);
+        c->ix = realloc(c->ix, (size_t)grown * sizeof *c->ix);
+        memset(c->ix + c->cap, ~0, (size_t)(grown - c->cap) * sizeof *c->ix);
+        c->cap = grown;
+    }
+
+    int ix = c->ix[id];
+    if (ix < 0) {
+        if (is_pow2_or_zero(c->n)) {
+            int const grown = c->n ? 2*c->n : 1;
+            c->data = realloc(c->data, (size_t)grown * size);
+            c->id   = realloc(c->id,   (size_t)grown * sizeof *c->id);
+        }
+        ix = c->n++;
+        c->id[ix] = id;
+        c->ix[id] = ix;
+    }
+
+    return (char*)c->data + (size_t)ix * size;
+}
+
+static void* component_lookup_(struct component const *c, size_t size, int id) {
+    if (id < c->cap) {
+        int const ix = c->ix[id];
+        if (ix >= 0) {
+            return (char*)c->data + (size_t)ix * size;
+        }
+    }
+    return NULL;
+}
 
 static void ensure(struct table *t) {
     if (!t->comp && t->columns) {
