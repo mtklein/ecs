a33104c784c8d087e49579f54e3a397cf5c5abf5
Mike Klein <mtklein@gmail.com>
2025-08-03 16:23:41 -0400

    Implement table API and tests (#74)
    * Implement table API
    
    * tweaks

--- table.h
+++ table.h
@@ -4,12 +4,16 @@
 #include <stddef.h>
 
 struct table {
-    size_t const *column;
+    size_t const     *column_size;
+    int               columns;
+    int               :32;
+    struct component *comp;
 };
+void drop_table(struct table *);
 
-_Bool lookup(struct table const*, int  id, void       *data, .../*columns*/);
-_Bool survey(struct table const*, int *id, void       *data, .../*columns*/);
-void  update(struct table      *, int  id, void const *data, .../*columns*/);
+_Bool lookup(struct table const*, int  id, void       *data, .../*columns,~0*/);
+_Bool survey(struct table const*, int *id, void       *data, .../*columns,~0*/);
+void  update(struct table      *, int  id, void const *data, .../*columns,~0*/);
 
 _Bool vlookup(struct table const*, int  id, void       *data, va_list columns);
 _Bool vsurvey(struct table const*, int *id, void       *data, va_list columns);
--- table_test.c
+++ table_test.c
@@ -1,6 +1,8 @@
 #include "table.h"
 #include "test.h"
 
+#define len(x) (int)(sizeof (x) / sizeof *(x))
+
 static void test_basics(void) {
     struct pos {
         float x,y;
@@ -10,18 +12,18 @@ static void test_basics(void) {
     };
 
     enum {POS, STATS};
-    size_t const column[] = {
+    size_t const column_size[] = {
         [POS]   = sizeof(struct pos),
         [STATS] = sizeof(struct stats),
     };
 
-    struct table t = { .column = column };
+    struct table t = { .column_size = column_size, .columns = len(column_size) };
     int next_id = 0;
 
     {
         int const id = next_id++;
         struct pos pos = {3,4};
-        update(&t,id, &pos, POS);
+        update(&t,id, &pos, POS,~0);
     }
     {
         int const id = next_id++;
@@ -29,18 +31,18 @@ static void test_basics(void) {
             struct pos   pos;
             struct stats stats;
         } cols = {{1,2}, {10,14,2,7}};
-        update(&t,id, &cols, POS,STATS);
+        update(&t,id, &cols, POS,STATS,~0);
     }
 
     struct pos   pos;
     struct stats stats;
-    expect( lookup(&t,0,&pos  ,POS  ));
-    expect(!lookup(&t,0,&stats,STATS));
-    expect( lookup(&t,1,&pos  ,POS  ));
-    expect( lookup(&t,1,&stats,STATS));
+    expect( lookup(&t,0,&pos  ,POS  ,~0));
+    expect(!lookup(&t,0,&stats,STATS,~0));
+    expect( lookup(&t,1,&pos  ,POS  ,~0));
+    expect( lookup(&t,1,&stats,STATS,~0));
 
     int n = 0;
-    for (int id = ~0; survey(&t,&id, &pos, POS);) {
+    for (int id = ~0; survey(&t,&id, &pos, POS,~0);) {
         n++;
     }
     expect(n == 2);
@@ -51,15 +53,36 @@ static void test_basics(void) {
     } join;
 
     n = 0;
-    for (int id = ~0; survey(&t,&id, &join, STATS,POS);) {
+    for (int id = ~0; survey(&t,&id, &join, STATS,POS,~0);) {
         expect(id == 1);
-        expect(equiv(join.pos.x, 3));
+        expect(equiv(join.pos.x, 1));
         expect(join.stats.ac == 14);
         n++;
     }
     expect(n == 1);
 
-    // TODO: test update() too
+    {
+        struct stats s = {20,30,3,4};
+        update(&t,0, &s, STATS,~0);
+        expect( lookup(&t,0,&stats, STATS,~0));
+        expect(stats.hp == 20);
+    }
+    {
+        struct { struct pos pos; struct stats stats; } patch = {{6,7}, {1,2,3,4}};
+        update(&t,1, &patch, POS,STATS,~0);
+        struct { struct stats stats; struct pos pos; } got;
+        expect( lookup(&t,1,&got, STATS,POS,~0));
+        expect(equiv(got.pos.x, 6));
+        expect(got.stats.hp == 1);
+    }
+
+    n = 0;
+    for (int id = ~0; survey(&t,&id, &join, STATS,POS,~0);) {
+        n++;
+    }
+    expect(n == 2);
+
+    drop_table(&t);
 }
 
 int main(void) {
