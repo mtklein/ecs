c211c7f1b9a2e1830e059bc53d2dc72482b71836
Mike Klein <mtklein@gmail.com>
2025-08-05 19:12:37 -0400

    all together

--- column.h
+++ column.h
@@ -1,15 +1,12 @@
 #pragma once
 
-struct column_vtable;
-
 struct column {
-    struct column_vtable const *vptr;
+    struct column_vtable {
+        void  (*  drop)(struct column      *);
+        void  (*attach)(struct column      *, int  id, void const*);
+        _Bool (*  find)(struct column const*, int  id, void      *);
+        void  (*detach)(struct column      *, int  id);
+        _Bool (*  walk)(struct column const*, int *id);
+    } const *vptr;
 };
 
-struct column_vtable {
-    void  (*  drop)(struct column      *);
-    void  (*attach)(struct column      *, int  id, void const*);
-    _Bool (*  find)(struct column const*, int  id, void      *);
-    void  (*detach)(struct column      *, int  id);
-    _Bool (*  walk)(struct column const*, int *id);
-};
