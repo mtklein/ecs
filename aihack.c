cdf6d06f4a94795327a79797ba1cd4e45aea818c
Mike Klein <mtklein@gmail.com>
2025-08-06 08:35:58 -0400

    style

--- aihack.c
+++ aihack.c
@@ -6,11 +6,6 @@
 #include <termios.h>
 #include <unistd.h>
 
-static int const nil = 0;
-static int next_id = 1;
-static int event_first;
-static int event_last;
-
 struct pos {
     int x,y;
 };
@@ -20,10 +15,12 @@ struct stats {
 enum disposition {
     LEADER, PARTY, FRIENDLY, NEUTRAL, HOSTILE, MADDENED
 };
+static struct column *pos,
+                     *stats,
+                     *glyph,
+                     *disp;
+static int next_entity = 1;
 
-enum game_state {
-    RUNNING, DIED, QUIT
-};
 
 struct key_event {
     int key;
@@ -33,33 +30,15 @@ struct attack_event {
 };
 struct config_event {
     int w,h;
-    enum game_state *game_state;
+    enum game_state {RUNNING,DIED,QUIT} *game_state;
     int (*d20)(void *rng);
     void *rng;
 };
+static struct column *key_event,
+                     *attack_event,
+                     *config_event;
+static int next_event = 0;
 
-static struct column *pos;
-static struct column *stats;
-static struct column *glyph;
-static struct column *disp;
-static struct column *key_event;
-static struct column *attack_event;
-static struct column *config_event;
-
-static void drop_columns(void) {
-    struct column *col[] = {pos,stats,glyph,disp,key_event,attack_event,config_event};
-    for (int i = 0; i < len(col); i++) {
-        if (col[i]) {
-            col[i]->vptr->drop(col[i]);
-        }
-    }
-}
-
-#define queue_event(col, ...) \
-    do { \
-        __typeof__((__VA_ARGS__)) tmp = (__VA_ARGS__); \
-        update(event_last++, col,&tmp); \
-    } while (0)
 
 static int entity_at(int x, int y) {
     struct pos p;
@@ -68,7 +47,7 @@ static int entity_at(int x, int y) {
             return id;
         }
     }
-    return nil;
+    return 0;
 }
 
 static struct attack_event try_move(int dx, int dy, int w, int h) {
@@ -95,30 +74,19 @@ static struct attack_event try_move(int dx, int dy, int w, int h) {
     return (struct attack_event){0};
 }
 
-static unsigned rng(unsigned seed) {
-    __builtin_mul_overflow(seed, 1103515245u, &seed);
-    __builtin_add_overflow(seed,      12345u, &seed);
-    return seed;
-}
-static int d20(void *ctx) {
-    unsigned *seed = ctx;
-    *seed = rng(*seed);
-    return 1 + (int)(*seed % 20);
-}
-
 static void game_state_system(int event) {
     static enum game_state *game_state;
 
-    struct config_event c;
-    if (lookup(event, config_event,&c)) {
-        game_state = c.game_state;
+    for (struct config_event e; lookup(event,config_event,&e);) {
+        game_state = e.game_state;
+        break;
     }
 
-    struct key_event k;
-    if (lookup(event, key_event,&k)) {
-        if (k.key == 'q') {
+    for (struct key_event e; lookup(event,key_event,&e);) {
+        if (e.key == 'q') {
             *game_state = QUIT;
         }
+        break;
     }
 
     struct stats     s;
@@ -133,16 +101,15 @@ static void game_state_system(int event) {
 static void movement_system(int event) {
     static int w,h;
 
-    struct config_event c;
-    if (lookup(event, config_event,&c)) {
-        w = c.w;
-        h = c.h;
+    for (struct config_event e; lookup(event,config_event,&e);) {
+        w = e.w;
+        h = e.h;
+        break;
     }
 
-    struct key_event k;
-    if (lookup(event, key_event,&k)) {
+    for (struct key_event e; lookup(event,key_event,&e);) {
         int dx=0, dy=0;
-        switch (k.key) {
+        switch (e.key) {
             default: return;
             case 'h': dx=-1; break;
             case 'j': dy=+1; break;
@@ -152,8 +119,9 @@ static void movement_system(int event) {
 
         struct attack_event a = try_move(dx,dy, w,h);
         if (a.defender) {
-            queue_event(attack_event, a);
+            update(next_event++, attack_event, &a);
         }
+        break;
     }
 }
 
@@ -161,44 +129,43 @@ static void combat_system(int event) {
     static int (*d20)(void *rng);
     static void *rng;
 
-    struct config_event c;
-    if (lookup(event, config_event,&c)) {
-        d20 = c.d20;
-        rng = c.rng;
+    for (struct config_event e; lookup(event,config_event,&e);) {
+        d20 = e.d20;
+        rng = e.rng;
+        break;
     }
 
-    struct attack_event a;
-    if (lookup(event, attack_event,&a)) {
+    for (struct attack_event e; lookup(event, attack_event,&e);) {
         struct stats as, ds;
-        _Bool const has_as = lookup(a.attacker, stats,&as);
-        _Bool const has_ds = lookup(a.defender, stats,&ds);
+        _Bool const has_as = lookup(e.attacker, stats,&as);
+        _Bool const has_ds = lookup(e.defender, stats,&ds);
         if (has_as && has_ds) {
             int const roll = d20(rng);
             if (roll > 1) {
                 if (roll == 20 || roll + as.atk >= ds.ac) {
                     ds.hp -= as.dmg;
                     if (ds.hp <= 0) {
-                        char x = 'x';
-                        update(a.defender, glyph,&x);
-                        erase(a.defender, stats, disp);
+                        update(e.defender, glyph,&(char){'x'});
+                        erase(e.defender, stats, disp);
                     } else {
-                        update(a.defender, stats,&ds);
+                        update(e.defender, stats,&ds);
                     }
                 }
             }
         } else if (has_as && !has_ds) {
-            erase(a.defender, pos, glyph);
+            erase(e.defender, pos, glyph);
         }
+        break;
     }
 }
 
 static void draw_system(int event) {
     static int w,h;
 
-    struct config_event c;
-    if (lookup(event, config_event,&c)) {
-        w = c.w;
-        h = c.h;
+    for (struct config_event e; lookup(event, config_event,&e);) {
+        w = e.w;
+        h = e.h;
+        break;
     }
 
     printf("\033[H");
@@ -226,31 +193,39 @@ static void draw_system(int event) {
     }
 }
 
-static void drain_events(void (*system[])(int), int systems) {
-    for (int event = event_first; event < event_last; event++) {
-        for (int i = 0; i < systems; i++) {
+static void drain_events(void) {
+    static void (*system[])(int) = {
+        game_state_system,
+          movement_system,
+            combat_system,
+              draw_system,
+    };
+
+    for (int event = 0; event < next_event; event++) {
+        for (int i = 0; i < len(system); i++) {
             system[i](event);
         }
     }
-    event_first = event_last;
+    for (int event = 0; event < next_event; event++) {
+        erase(event, key_event, attack_event, config_event);
+    }
+    next_event = 0;
 }
 
-static void reset_terminal(void) {
-    printf("\033[?1049l");
+static unsigned rng(unsigned seed) {
+    __builtin_mul_overflow(seed, 1103515245u, &seed);
+    __builtin_add_overflow(seed,      12345u, &seed);
+    return seed;
+}
+static int d20(void *ctx) {
+    unsigned *seed = ctx;
+    *seed = rng(*seed);
+    return 1 + (int)(*seed % 20);
 }
 
 int main(int argc, char const* argv[]) {
-    int const w=10, h=5;
     unsigned seed = (unsigned)(argc > 1 ? atoi(argv[1]) : 0);
-
-    pos         = sparse_column(sizeof(struct pos));
-    stats       = sparse_column(sizeof(struct stats));
-    glyph       = sparse_column(sizeof(char));
-    disp        = sparse_column(sizeof(enum disposition));
-    key_event   = sparse_column(sizeof(struct key_event));
-    attack_event= sparse_column(sizeof(struct attack_event));
-    config_event= sparse_column(sizeof(struct config_event));
-    atexit(drop_columns);
+    int const w=10, h=5;
 
     {
         struct termios termios;
@@ -260,35 +235,34 @@ int main(int argc, char const* argv[]) {
     }
     printf("\033[?1049h");
     printf("\033[?25l");
-    atexit(reset_terminal);
 
-    update(next_id++, pos,   &(struct pos){1,1}
-                    , stats, &(struct stats){10,10,2,4}
-                    , glyph, &(char){'@'}
-                    , disp,  &(enum disposition){LEADER});
-    update(next_id++, pos,   &(struct pos){3,1}
-                    , stats, &(struct stats){4,12,3,2}
-                    , glyph, &(char){'i'}
-                    , disp,  &(enum disposition){HOSTILE});
+    pos   = sparse_column(sizeof(struct pos));
+    stats = sparse_column(sizeof(struct stats));
+    glyph = sparse_column(sizeof(char));
+    disp  = sparse_column(sizeof(enum disposition));
 
-    void (*system[])(int) = {
-        game_state_system,
-          movement_system,
-            combat_system,
-              draw_system,
-    };
+    key_event    = sparse_column(sizeof(struct key_event));
+    attack_event = sparse_column(sizeof(struct attack_event));
+    config_event = sparse_column(sizeof(struct config_event));
+
+    update(next_entity++, pos,   &(struct pos){1,1}
+                        , stats, &(struct stats){10,10,2,4}
+                        , glyph, &(char){'@'}
+                        , disp,  &(enum disposition){LEADER});
+    update(next_entity++, pos,   &(struct pos){3,1}
+                        , stats, &(struct stats){4,12,3,2}
+                        , glyph, &(char){'i'}
+                        , disp,  &(enum disposition){HOSTILE});
 
     enum game_state game_state = RUNNING;
-
-    {
-        queue_event(config_event, (struct config_event){w,h,&game_state,d20,&seed});
-        drain_events(system, len(system));
-    }
+    update(next_event++, config_event, &(struct config_event){w,h,&game_state,d20,&seed});
+    drain_events();
 
     while (game_state == RUNNING) {
-        queue_event(key_event, (struct key_event){getchar()});
-        drain_events(system, len(system));
+        update(next_event++, key_event, &(struct key_event){getchar()});
+        drain_events();
     }
+
+    printf("\033[?1049l");
     return game_state == DIED ? 1 : 0;
 }
-
